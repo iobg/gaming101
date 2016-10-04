e9881ed5edee82ef3d6432f30c9941fc121975e5
Banks Humphreys <iobg0320@gmail.com>
2016-10-04 15:18:27 -0500

    win condition added

--- public/main.js
+++ public/main.js
@@ -32,20 +32,52 @@ const drawBoard=(boardState)=>{
 	</table>
 	`
 }
+
+const winner = b => {
+  // Rows
+  if (b[0][0] && b[0][0] === b[0][1] && b[0][1] === b[0][2]) {
+    return b[0][0]
+  } else if (b[1][0] && b[1][0] === b[1][1] && b[1][1] === b[1][2]) {
+    return b[1][0]
+  } else if (b[2][0] && b[2][0] === b[2][1] && b[2][1] === b[2][2]) {
+    return b[2][0]
+  }
+
+  // Cols
+  else if (b[0][0] && b[0][0] === b[1][0] && b[1][0] === b[2][0]) {
+    return b[0][0]
+  } else if (b[0][1] && b[0][1] === b[1][1] && b[1][1] === b[2][1]) {
+    return b[0][1]
+  } else if (b[0][2] && b[0][2] === b[1][2] && b[1][2] === b[2][2]) {
+    return b[0][2]
+  }
+
+  // Diags
+  else if (b[0][0] && b[0][0] === b[1][1] && b[1][1] === b[2][2]) {
+    return b[0][0]
+  } else if (b[0][2] && b[0][2] === b[1][1] && b[1][1] === b[2][0]) {
+    return b[0][2]
+  }
+}
+
+
 const table= document.querySelector('.board')
 let nextPlayer = 'X'
 
 table.addEventListener('click',event=>{
 	const col = event.target.cellIndex
 	const row = event.target.parentElement.rowIndex
-	
-	if(!board[row][col]){
+
+	if(board[row][col]){
+		console.log("You can't play there")
+	}
+	else{
 		board[row][col]=nextPlayer
 		nextPlayer= nextPlayer === 'X' ? 'O': 'X'
 		drawBoard(board)
 	}
-	else{
-		console.log("You can't play there")
+	if(winner(board)){
+		console.log(`${winner(board)} has won`)
 	}
 	
 })
