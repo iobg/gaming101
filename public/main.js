9e632d9186d8935277fa2a7916713db929322eee
Banks Humphreys <iobg0320@gmail.com>
2016-10-04 14:55:39 -0500

    set up board as multidimensional array

--- public/main.js
+++ public/main.js
@@ -4,3 +4,18 @@ const socket = io()
 
 socket.on('connect',()=>console.log(`Socket connected: ${socket.id}`))
 socket.on('disconnect',()=>console.log(`Socket disconnected: ${socket.id}`))
+
+const table = document.querySelector('table')
+const board=[
+	[,,],
+	[,,],
+	[,,]
+]
+table.addEventListener('click',event=>{
+	const col = event.target.cellIndex
+	const row = event.target.parentElement.rowIndex
+	board[row][col]='O'
+	console.log(board)
+	event.target.innerText= 'O'
+	console.log(`You clicked on r${row} c${col}`)
+})
